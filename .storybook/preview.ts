2a9a6b71c5f260a4f356dcebe506fb1a937661cb
Siddamnandas <128347410+Siddamnandas@users.noreply.github.com>
2025-08-21 21:49:14 +0530

    feat: add storybook setup and example stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,9 @@ import type { Preview } from 'storybook';
 const preview: Preview = {
   parameters: {
     chromatic: { viewports: [320, 768, 1024] },
+    a11y: {
+      element: '#root',
+    },
   },
 };
 
